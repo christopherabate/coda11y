04201bdf340cafc53e2a7c329b4ee9fce8acb499
Christopher Abate <contact@christopherabate.com>
2024-02-08 01:57:00 +0100

    Add files via upload

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,89 +1,82 @@
-// Function to update the results
-function updateResult (box) {
-  let result = box.querySelector("iFrame");
-  let html = box.querySelector(".language-html");
-  let css = box.querySelector(".language-css");
-  let js = box.querySelector(".language-js");
+class Sandbox {
+  constructor(options = {}) {
+    this.selector = options.selector || "#SandboxExemple1";
+    this.editable = options.editable || true;
+    
+    this.template = document.querySelector(`${this.selector} iframe`).textContent;
+    
+    this.updateOutput();
+    
+    if (this.editable === true) {
+      // Collect codeboxes
+      let codeboxes = document.querySelectorAll(`${this.selector} pre code`);
+    
+      // Parse codeboxes
+      codeboxes.forEach((codebox) => {
+      
+        // Create the editor div
+        let editor = document.createElement("div");
+        editor.classList.add("sandbox-editor");
+        
+        // Create a textarea for edition
+        let textarea = document.createElement("textarea");
+        textarea.setAttribute("rows", "1");
+        textarea.setAttribute("spellcheck", "false");
+        textarea.setAttribute("autocorrect", "off");
+        textarea.setAttribute("autocomplete", "off");
+        textarea.setAttribute("autocapitalize", "off");
+        textarea.setAttribute("translate", "no");
+        textarea.setAttribute("title", codebox.getAttribute("title"));
+        textarea.setAttribute("aria-label", codebox.getAttribute("title"));
+        textarea.innerHTML = codebox.textContent;
+        
+        // Move textarea and code into editor
+        codebox.parentElement.insertAdjacentElement("beforebegin", editor);
+        editor.appendChild(codebox.parentElement);
+        editor.appendChild(textarea);
+        
+        // Timeout
+        let timeoutID;
+        
+        // On change
+        textarea.addEventListener("input", (e) => {
+          codebox.textContent = e.target.value;
+          
+          try {
+            Prism.highlightAll();
+          } catch (error) {
+            console.log(error);
+          }
+          
+          clearTimeout(timeoutID);
+          timeoutID = setTimeout(() => {
+            this.updateOutput();;
+          }, 400);
+        })
+      });
+    }
+  }
   
-  // Create new iframe
-  let clone = result.cloneNode();
-  result.replaceWith(clone);
-  result = clone;
-
-  // Render
-  result.contentWindow.document.open();
-  result.contentWindow.document.writeln(`
-    <!doctype html>
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <style>${css.textContent}<\/style>
-      <\/head>
-      <body>
-        ${html.textContent}
-        <script type="module">${js.textContent}<\/script>
-      <\/body>
-    <\/html>
-  `);
-  result.contentWindow.document.close();
+  updateOutput() {
+    let input = {
+      html: document.querySelector(`${this.selector} .language-html`).textContent,
+      css: document.querySelector(`${this.selector} .language-css`).textContent,
+      js: document.querySelector(`${this.selector} .language-js`).textContent,
+    };
+    
+    let output = document.querySelector(`${this.selector} iframe`);
+    let languages = Object.keys(input);
+    let code = Object.values(input);
+    
+    let render = new Function(...languages, `return \`${this.template}\`;`)(...code);
+		let clone = output.cloneNode();
+		output.replaceWith(clone);
+		output = clone;
+    
+    output.contentWindow.document.open();
+    output.contentWindow.document.writeln(render);
+    output.contentWindow.document.close();
+  }
 }
 
-// Collect sandboxes
-let sandboxes = document.querySelectorAll('.sandbox');
-
-// Parse sandboxes
-sandboxes.forEach((sandbox) => {
-
-  updateResult(sandbox);
-  
-  // Check for editable mode
-  if (sandbox.getAttribute("data-sandbox-editable") === "true") {
-  
-    // Collect codeboxes
-    let codeboxes = sandbox.querySelectorAll('pre code');
-  
-    // Parse codeboxes
-    codeboxes.forEach((codebox) => {
-    
-      // Create the editor div
-      let editor = document.createElement("div");
-      editor.classList.add("sandbox-editor");
-      
-      // Create a textarea for edition
-      let textarea = document.createElement("textarea");
-      textarea.setAttribute("rows", "1");
-      textarea.setAttribute("spellcheck", "false");
-      textarea.setAttribute("autocorrect", "off");
-      textarea.setAttribute("autocomplete", "off");
-      textarea.setAttribute("autocapitalize", "off");
-      textarea.setAttribute("translate", "no");
-      textarea.setAttribute("title", codebox.getAttribute("title"));
-      textarea.setAttribute("aria-label", codebox.getAttribute("title"));
-      textarea.innerHTML = codebox.textContent;
-      
-      // Move textarea and code into editor
-      codebox.parentElement.insertAdjacentElement("beforebegin", editor);
-      editor.appendChild(codebox.parentElement);
-      editor.appendChild(textarea);
-      
-      // Timeout
-      let timeoutID;
-      
-      // On change
-      textarea.addEventListener("input", (e) => {
-        codebox.textContent = e.target.value;
-        
-        try {
-          Prism.highlightAll();
-        } catch (error) {
-          console.log(error);
-        }
-        
-        clearTimeout(timeoutID);
-        timeoutID = setTimeout(() => {
-          updateResult(sandbox);
-        }, 400);
-      })
-    });
-  }
-});
+new Sandbox ();
