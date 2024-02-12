7acd3e5a3a7f44fb061c5d7eb57172c1fdd6b08e
Christopher Abate <contact@christopherabate.com>
2024-02-12 11:37:40 +0100

    Update splitviews.js

--- assets/js/splitviews.js
+++ assets/js/splitviews.js
@@ -8,48 +8,48 @@ const splitviews = (boxes = "") => {
     
     box.querySelectorAll(".handle").forEach((handle) => {
       
-        let left = handle.previousElementSibling;
-        let right = handle.nextElementSibling;
-        let x = 0;
-        let leftOrigin = 0;
-        let rightOrigin = 0;
-  
-        let mousedown = function(e) {
-          x = e.clientX;
-          
-          leftOrigin = left.getBoundingClientRect().width;
-          rightOrigin = right.getBoundingClientRect().width;
+      let left = handle.previousElementSibling;
+      let right = handle.nextElementSibling;
+      let x = 0;
+      let leftOrigin = 0;
+      let rightOrigin = 0;
+
+      let mousedown = function(e) {
+        x = e.clientX;
         
-          document.addEventListener('mousemove', mousemove);
-          document.addEventListener('mouseup', mouseup);
-        };
+        leftOrigin = left.getBoundingClientRect().width;
+        rightOrigin = right.getBoundingClientRect().width;
+      
+        document.addEventListener('mousemove', mousemove);
+        document.addEventListener('mouseup', mouseup);
+      };
+      
+      let mousemove = function(e) {
+        const dx = e.clientX - x;
         
-        let mousemove = function(e) {
-          const dx = e.clientX - x;
-          
-          left.style.userSelect = "none";
-          left.style.pointerEvents = "none";
-          left.style.flexGrow = 0;
-          left.style.flexBasis = `${leftOrigin + dx}px`;
-          right.style.userSelect = "none";
-          right.style.pointerEvents = "none";
-          right.style.flexGrow = 0;
-          right.style.flexBasis = `${rightOrigin - dx}px`;
-        };
+        left.style.userSelect = "none";
+        left.style.pointerEvents = "none";
+        left.style.flexGrow = 0;
+        left.style.flexBasis = `${leftOrigin + dx}px`;
+        right.style.userSelect = "none";
+        right.style.pointerEvents = "none";
+        right.style.flexGrow = 0;
+        right.style.flexBasis = `${rightOrigin - dx}px`;
+      };
+      
+      let mouseup = function(e) {
+        left.style.removeProperty("user-select");
+        left.style.removeProperty("pointer-events");
+        right.style.removeProperty("user-select");
+        right.style.removeProperty("pointer-events");
         
-        let mouseup = function(e) {
-          left.style.removeProperty("user-select");
-          left.style.removeProperty("pointer-events");
-          right.style.removeProperty("user-select");
-          right.style.removeProperty("pointer-events");
-          
-          document.removeEventListener('mousemove', mousemove);
-          document.removeEventListener('mouseup', mouseup)
-        };
-        
-        handle.addEventListener('mousedown', mousedown);
-      });
+        document.removeEventListener('mousemove', mousemove);
+        document.removeEventListener('mouseup', mouseup)
+      };
+      
+      handle.addEventListener('mousedown', mousedown);
     });
+  });
   
   return BOXES;
 }
