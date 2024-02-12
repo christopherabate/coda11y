e7ab5d696ae803cb65d1546dc45c208af896cf61
Christopher Abate <contact@christopherabate.com>
2024-02-12 15:31:53 +0100

    Update coda11y.js

--- assets/js/coda11y.js
+++ assets/js/coda11y.js
@@ -3,6 +3,18 @@ import splitviews from "./modules/splitviews.js";
 
 splitviews();
 
+const alert = (parent, title, message, type) => {
+  const wrapper = document.createElement("div")
+  wrapper.innerHTML = [`
+    <p class="alert alert-${type} alert-dismissible fade show" role="alert">
+      <strong>${title} :<\/strong> ${message}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Fermer"><\/button>
+    <\/p>`
+    ];
+
+  parent.appendChild(wrapper)
+}
+
 document.querySelectorAll("form").forEach((box) => {
   sandbox({ box: box, editable: true });
 
@@ -18,12 +30,15 @@ document.querySelectorAll("form").forEach((box) => {
       case "clue2":
       case "clue3":
         console.log("clue");
+        alert(box.querySelector("#console .card-body"), '<span class="bi bi-info-circle-fill"></span> Indice', "Nice, you triggered this alert message!", "info")
         break;
       case "solution":
         console.log("solution");
+        alert(box.querySelector("#console .card-body"), '<span class="bi bi-lightbulb-fill"></span> Solution', "Nice, you triggered this alert message!", "warning")
         break;
       case "validate":
         console.log("validate");
+        alert(box.querySelector("#console .card-body"), '<span class="bi bi-universal-access-circle"></span> Success', "Nice, you triggered this alert message!", "success")
         break;
     }
     
