1958f615ae8d607c3cafae589e11fee1cf8f02d7
Christopher Abate <contact@christopherabate.com>
2024-02-20 13:14:53 +0100

    Update coda11y.js

--- assets/js/coda11y.js
+++ assets/js/coda11y.js
@@ -77,7 +77,7 @@ Object.values(coda11y).forEach((question, index) => {
         </div>
       </form>
     `],
-    style.setProperty: ['--slide', index]
+    style: ['--slide', index]
   }));
 });
 
